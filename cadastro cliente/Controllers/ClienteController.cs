9d72ba8748ce89190952d1aafd8171d4f7313001
projetoscintia <cpinho@tecnologia-unica.com.br>
2023-06-21 14:59:13 -0300

    projetomvc

---
+++ cadastro cliente/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastro_cliente.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
